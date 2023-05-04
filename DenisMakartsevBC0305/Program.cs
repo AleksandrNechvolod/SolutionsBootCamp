8bc26a138be2f2fd9f2003f4f29d4a2dd2df26d1
Aleksandr Nechvolod <alexnechvolod@gmail.com>
2023-05-04 17:21:06 +0300

    First commit

---
+++ DenisMakartsevBC0305/Program.cs
@@ -0,0 +1,39 @@
+﻿//  Создание и вывод массива
+// int[] array = new int[5];
+// for (int i = 0; i < 5; i++)
+//   array[i] = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("[" + string.Join(", ", array) + "]");
+// Console.WriteLine(array[3]);
+// // Сложность алгоритма O(1)
+
+// нахождение суммы элементов массива
+// int n = 5;
+// int[] array = new int[n];
+// for (int i = 0; i < n; i++)
+//   array[i] = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("[" + string.Join(", ", array) + "]");
+// // [4, 5, 3, 1, 2]
+// // O(n)
+// // [1, 2, 3, 4, 5] - O(n * log n)
+// // ((5 + 1) / 2) * 5 - 0(1)
+// // n < n * log(n) + 1
+
+// //  а лучше решить вот так
+// int sum = 0;
+// for (int i = 0; i < n; i++)
+//   sum += array[i];
+// Console.WriteLine(sum);
+// // O(n)
+
+
+int n = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= n; i++)
+{
+  for (int j = 1; j <= n; j++)
+  {
+    Console.Write(i * j);
+    Console.Write("\t");
+  }
+  Console.WriteLine();
+}
+// O(n*2)
---
+++ Kamyanetskiy_Lec5_1/Program.cs
@@ -0,0 +1,33 @@
+﻿// Console.Clear();
+// int af=-5;
+// int uf=5;
+// Console.WriteLine($"{af}..{uf}");
+
+// using System;
+
+// class Hello
+// {
+//   static void Main()
+//   {
+//     // This line prints "Hello, World" 
+//     Console.WriteLine("Hello, World");
+//   }
+// }
+
+//   var a = 32;
+// Console.WriteLine(a.GetType().Name);
+
+// var data=new int[]{1,2,3,4,5}
+
+
+// using System.Linq;
+
+// string text = "(1,2) (3,4) (4,5) (67)";
+var data = text.Split(" ")
+/// Это комментарий
+ /// |  |  |  .ToArray();
+
+// for (int i = 0; i < data.Length; i++)
+// {
+//   Console.WriteLine(data[i]);
+// }
